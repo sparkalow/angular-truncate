0543c5c15992567e540d10c55bda94a8aa5d2d72
Julien Bouquillon <julien@bouquillon.com>
2013-05-28 00:03:05 +0200

    refactor(demo): add words filter demo

--- demo/js/app.js
+++ demo/js/app.js
@@ -5,10 +5,5 @@ var myApp = angular.module('myApp', ['truncate']);
 myApp.controller('demoController', function ($scope) {
     $scope.text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
     $scope.numChars = 20;
-
-
-
-//    $scope.$watch('numChars',function(newValue, oldValue){
-//        $scope.text = $scope.text;
-//    })
+    $scope.numWords = 5;
 });
