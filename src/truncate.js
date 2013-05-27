6bd3f2213d90558eaa34cecfa054447f2e9a6a0d
Julien Bouquillon <julien@bouquillon.com>
2013-05-27 23:42:59 +0200

    feat(words): add words filter+tests

--- src/truncate.js
+++ src/truncate.js
@@ -1,18 +1,30 @@
-angular.module('truncate',[]);
-angular.module('truncate')
+angular.module('truncate',[])
     .filter('characters', function() {
-    return function(input, chars) {
-        if(input && input.length >= chars)
-        {
-            input = input.substring(0, chars);
+        return function(input, chars) {
+            if(input && input.length >= chars)
+            {
+                input = input.substring(0, chars);
 
-            var lastspace = input.lastIndexOf(' ');
-            //get last space
-            if (lastspace != -1) {
-                input = input.substr(0, lastspace);
+                var lastspace = input.lastIndexOf(' ');
+                //get last space
+                if (lastspace !== -1) {
+                    input = input.substr(0, lastspace);
+                }
+                return input + '...';
             }
-            return input + '...';
-        }
-        return input;
-    }
-});
+            return input;
+        };
+    })
+    .filter('words', function() {
+        return function(input, words) {
+            if (words<=0) return '';
+            if(input)
+            {
+                var inputWords = input.split(/\s+/);
+                if (inputWords.length > words) {
+                    input = inputWords.slice(0, words).join(' ') + '...';
+                }
+            }
+            return input;
+        };
+    });
