055619f0bf8f965e70221ed3d1f09ff32ebf956b
Brian Matthews <git@slapthink.net>
2013-05-27 22:14:07 -0500

    merged pull request

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -58,7 +58,7 @@ autoWatch = true;
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-browsers = ['Chrome', 'Firefox'];
+browsers = ['Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
 // CLI --capture-timeout 5000
