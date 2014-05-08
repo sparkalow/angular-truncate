4fce25ec98a6cea0471b6916f8d9a7a8254e7a07
Sean Houghton <sean_houghton@playstation.sony.com>
2014-05-08 13:25:02 -0700

    Add an option to split strings in the middle instead of truncating at the end

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -94,4 +94,40 @@ describe('truncate', function () {
         });
 
     });
+
+    describe('splitcharacters', function () {
+        var characterFilter;
+
+        beforeEach(inject(function ($filter) {
+            characterFilter = $filter('splitcharacters');
+        }));
+
+        it('should do nothing to this string', function () {
+            expect(characterFilter('1234567890')).toEqual('1234567890');
+        });
+
+        it('should fail', function () {
+            expect(characterFilter(null, 30)).toNotEqual('1234567890');
+        });
+
+        it('should not trim these down', function () {
+            expect(characterFilter('1234567890', 30)).toEqual('1234567890');
+        });
+
+        it('should trim these down', function () {
+            expect(characterFilter('1234567890', 5)).toEqual('12...890');
+        });
+
+        it('should trim this down including the space', function () {
+            expect(characterFilter('123456789 10 11 12 13 14', 13)).toEqual('123456...2 13 14');
+        });
+
+        it('should handle invalid numbers', function () {
+            expect(characterFilter('1234567890', 0)).toEqual('');
+        });
+
+        it('should handle invalid chars numbers type', function () {
+            expect(characterFilter('1234567890', 'abc')).toEqual('1234567890');
+        });
+    });
 });
